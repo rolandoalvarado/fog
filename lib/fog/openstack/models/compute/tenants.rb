9c7cf2d723cdf0f1120a7654bcca6f8f886db677
Alfonso Juan Dillera <adillera@morphlabs.com>
2012-04-23 21:49:54 +0800

    Added find_by_id in tenant in computes.

--- lib/fog/openstack/models/compute/tenants.rb
+++ lib/fog/openstack/models/compute/tenants.rb
@@ -11,6 +11,12 @@ module Fog
           load(connection.list_tenants.body['tenants'])
         end
 
+        def find_by_id(id)
+          self.find {|tenant| tenant.id == id} ||
+            Fog::Compute::OpenStack::Tenant.new(
+              connection.get_tenant(id).body['tenant'])
+        end
+
         def usages(start_date = nil, end_date = nil, details = false)
           connection.list_usages(start_date, end_date, details).body['tenant_usages']
         end 
