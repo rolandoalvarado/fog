322cf464532215a8d178e02884a6a20a84d46dab
Alvin Garcia <agarcia1337@gmail.com>
2012-09-14 19:05:18 +0800

    [openstack|identity] Added flexibility for a list roles mock

--- lib/fog/openstack/requests/identity/list_roles_for_user_on_tenant.rb
+++ lib/fog/openstack/requests/identity/list_roles_for_user_on_tenant.rb
@@ -13,10 +13,13 @@ module Fog
 
       class Mock
         def list_roles_for_user_on_tenant(tenant_id, user_id)
+          is_admin = self.data[:users][user_id]['name'] == 'admin'
+          role_name = is_admin ? 'admin' : 'Member'
+
           response = Excon::Response.new
           response.status = 200
           response.body = {
-            'roles' => [{'id'=> user_id, 'name' => 'Member'}]
+            'roles' => [{'id'=> user_id, 'name' => role_name}]
           }
           response
         end # def list_roles_for_user_on_tenant
